0b29a4504ae8d107705fa571143eddd1e7ae6cbb
theeyow <tbjuliano@up.edu.ph>
2026-05-03 22:11:44 +0800

    refactor: change integer constants to macros

--- colorgame.c
+++ colorgame.c
@@ -3,26 +3,28 @@
 #include <time.h>
 #include "casino.h"
 
-void print_cube(char colors[][9], int cube_size, int chosen_color);
+#define COLOR_CHOICES 6
+#define COLOR_CODE_WIDTH 9
+#define CUBE_HEIGHT 10
+#define CUBE_COUNT 3
+
+void print_cube(char colors[COLOR_CHOICES][COLOR_CODE_WIDTH], int cube_size, int chosen_color);
 
 void playColorGame(User *user){
-	int color_choices = 6;
 	// Fetches ANSI color codes for cube faces and reset escape code.
-	char colors[6][9] = {
+	char colors[COLOR_CHOICES][COLOR_CODE_WIDTH] = {
 		RED, GRN, YEL, 
 		BLU, MAG, CYN
 	};
 	
-	// Sets cube height, number of cubes
-	int cube_size = 10; int cubes = 3; 
-	// Stores random colors for comparison
-	int random_colors[cubes];
+	// Storage of random colors for comparison
+	int random_colors[CUBE_COUNT];
 	
-	for(int i = 0; i<cubes; i++){
+	for(int i = 0; i<CUBE_COUNT; i++){
 		// Chooses random color for the ith cube
-		random_colors[i] = rand()%color_choices;
+		random_colors[i] = rand()%COLOR_CHOICES;
 		printf("\nCube %d | Color: %d\n", i+1, random_colors[i]);
-		print_cube(colors, cube_size, random_colors[i]);
+		print_cube(colors, CUBE_HEIGHT, random_colors[i]);
 	}
 }
 

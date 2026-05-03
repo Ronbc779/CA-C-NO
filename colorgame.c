30053c10c002613fdbf448d9a1e632812d87c0a5
theeyow <tbjuliano@up.edu.ph>
2026-05-03 22:49:22 +0800

    feat: add instructions for colorgame

--- colorgame.c
+++ colorgame.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+#include <stdbool.h>
 #include "casino.h"
 
 #define COLOR_CHOICES 6
@@ -16,16 +17,29 @@ void playColorGame(User *user){
 		RED, GRN, YEL, 
 		BLU, MAG, CYN
 	};
-	
+
 	// Storage of random colors for comparison
-	int random_colors[CUBE_COUNT];
-	
-	for(int i = 0; i<CUBE_COUNT; i++){
+	while(true){
+		int random_colors[CUBE_COUNT];
+		printf("\n====== COLOR GAME ======="
+				"\n1. Pick one color from the list"
+				"\n2. Wait for the roll to happen, for hits:"
+				"\n\ta. One (1) color: x1.5"
+				"\n\tb. Two (1) colors: x2.0"	
+				"\n\tc. Three (3) colors: x3.0"	
+				"\n3. Minimum bet: 50 coins"
+		);
+		for(int i = 0; i<CUBE_COUNT; i++){
 		// Chooses random color for the ith cube
 		random_colors[i] = rand()%COLOR_CHOICES;
 		printf("\nCube %d | Color: %d\n", i+1, random_colors[i]);
 		print_cube(colors, CUBE_HEIGHT, random_colors[i]);
+		}
+		break;
+
 	}
+	
+	
 }
 
 void print_cube(char colors[COLOR_CHOICES][COLOR_CODE_WIDTH], int cube_size, int chosen_color){

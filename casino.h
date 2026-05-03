7d37ca6fa7b020e2abbe9cf84e2e01dd6be61f15
Ron <rbcabungcag@up.edu.ph>
2026-05-03 13:52:14 +0800

    added more colors to casino.h

--- casino.h
+++ casino.h
@@ -1,11 +1,15 @@
 #ifndef CASINO_H
 #define CASINO_H
 
-#define RED   "\033[31m"
-#define BLK   "\033[30m"
-#define RESET "\033[0m"
-#define YLW "\033[0;33m"
-#define BOLD  "\033[1m"
+#define BLK "\e[0;30m"
+#define RED "\e[0;31m"
+#define GRN "\e[0;32m"
+#define YEL "\e[0;33m"
+#define BLU "\e[0;34m"
+#define MAG "\e[0;35m"
+#define CYN "\e[0;36m"
+#define RESET "\e[0m"
+#define BOLD  "\e[1m"
 
 typedef struct {
     char name[50];
